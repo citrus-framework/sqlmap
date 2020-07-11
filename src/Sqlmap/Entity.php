b64624da19a05b9d580008347d9cfcc4d0453a26
kouhei.takemoto <take64@citrus.tk>
2020-07-12 04:03:59 +0900

    entityインターフェースではなくtraitで良さそうだったのでそれで実装

---
+++ src/Sqlmap/Entity.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright   Copyright 2020, CitrusSqlmap. All Rights Reserved.
+ * @author      take64 <take64@citrus.tk>
+ * @license     http://www.citrus.tk/
+ */
+
+namespace Citrus\Sqlmap;
+
+/**
+ * 共通Entity
+ */
+trait Entity
+{
+    /** @var Condition */
+    public $condition;
+
+    /** @var string */
+    public $condition_class;
+
+
+
+    /**
+     * Conditionを取得
+     *
+     * @return Condition
+     */
+    public function getCondition(): Condition
+    {
+        return $this->condition;
+    }
+
+
+
+    /**
+     * Conditionを生成して返却
+     *
+     * @return Condition
+     */
+    public function callCondition(): Condition
+    {
+        if (true === is_null($this->condition))
+        {
+            $condition_class = $this->condition_class;
+            $this->condition = new $condition_class();
+            $this->condition->nullify();
+        }
+        if (true === method_exists($this, 'callPrimaryKeys'))
+        {
+            $primary_keys = $this->callPrimaryKeys();
+            foreach ($primary_keys as $primary_key)
+            {
+                $this->condition->$primary_key = $this->$primary_key;
+            }
+        }
+
+        return $this->condition;
+    }
+}
